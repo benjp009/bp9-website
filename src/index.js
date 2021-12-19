21637d95753c777edd546b3ea3afa88a22deeb83
benjp009 <patin.benjamin@gmail.com>
2021-12-19 19:43:17 +0100

    Work on navigation design

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 import '@fortawesome/fontawesome-free/js/fontawesome'
-
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 // function component() {
 //   const element = document.createElement('div');
